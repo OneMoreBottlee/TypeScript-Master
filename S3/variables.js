ad166ad8c53c1dfa03f27c87fbf80af1cf016fa9
OneMoreBottlee <hanbj0301@gmail.com>
2023-01-24 22:48:12 +0900

    sec3 : 컴파일링

---
+++ S3/variables.js
@@ -0,0 +1,15 @@
+// String
+var movieTitle = "Amadeus";
+movieTitle = "Harry Potter";
+// Error
+movieTitle = 9;
+// Number
+var numCatLives = 9;
+numCatLives += 1;
+// Error
+numCatLives = "Zero";
+// Boolean
+var gameOver = false;
+gameOver = true;
+// Error
+gameOver = "true";
--- S3/variables.ts
+++ S3/variables.ts
@@ -1,17 +1,19 @@
+// String
 let movieTitle: string = "Amadeus";
 movieTitle = "Harry Potter";
-
 // Error
 movieTitle = 9;
 
+
+// Number
 let numCatLives: number = 9
 numCatLives += 1
-
 // Error
 numCatLives = "Zero"
 
+
+// Boolean
 let gameOver : boolean = false
 gameOver = true
-
 // Error
 gameOver= "true"
