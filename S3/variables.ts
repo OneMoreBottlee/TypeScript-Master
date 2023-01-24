33e31df3c9db9820b37fb2f2715d2279a28ed4d8
OneMoreBottlee <hanbj0301@gmail.com>
2023-01-24 22:44:38 +0900

    sec3 : nubmer & boolean

--- S3/variables.ts
+++ S3/variables.ts
@@ -4,3 +4,14 @@ movieTitle = "Harry Potter";
 // Error
 movieTitle = 9;
 
+let numCatLives: number = 9
+numCatLives += 1
+
+// Error
+numCatLives = "Zero"
+
+let gameOver : boolean = false
+gameOver = true
+
+// Error
+gameOver= "true"
