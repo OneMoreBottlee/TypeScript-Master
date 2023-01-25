bdf81b312915d4da6d5f05f34a9d7cbec9179ce6
OneMoreBottlee <hanbj0301@gmail.com>
2023-01-25 15:17:31 +0900

    sec6 : 다차원 배열 타입

--- S6/arrays.ts
+++ S6/arrays.ts
@@ -3,3 +3,26 @@ activeUsers.push("steven");
 
 const ageList: number[] = [];
 ageList.push(135);
+
+// 또다른 선언 방법
+const boos: Array<boolean> = [];
+// const booa: boolean[] = []
+
+// 커스텀으로도 선언할 수 있음
+type Point = {
+  x: number;
+  y: number;
+};
+
+const coords: Point[] = [];
+coords.push({ x: 23, y: 8 });
+
+
+// 다차원 배열
+const board: string[][] = [
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+    ["X", "O", "X"]
+]
+
+const demo: number [][][] = [[[1]]]
