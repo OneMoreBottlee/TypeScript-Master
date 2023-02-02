eb00dc2fa92f4cc76a56f9eef3b13ddfae6aaf3a
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-02 17:13:04 +0900

    sec8 : Enum 개요

--- S8/tuplesEnums.ts
+++ S8/tuplesEnums.ts
@@ -1,14 +1,30 @@
+// Tuple
 
+let RGB: [number, number, number] = [255, 5, 5];
+RGB = [2, 5, 5];
+RGB = [255, 0, "255"];
 
-let RGB: [number, number, number] = [255, 5, 5]
-RGB = [2,5,5]
-RGB = [255, 0, "255"]
+type HTTPResponse = [number, string];
 
-type HTTPResponse = [number, string]
+const goodRes: HTTPResponse = [200, "OK"];
+goodRes[0] = "Res";
+goodRes.push(123);
 
-const goodRes: HTTPResponse = [200, "OK"]
-goodRes[0] = "Res"
-goodRes.push(123)
+const BadRes: HTTPResponse = ["Bad", 500];
 
-const BadRes: HTTPResponse= ["Bad", 500]
+// Enum
 
+enum OrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIVERED,
+  RETURNED,
+}
+
+const myStatus = OrderStatus.DELIVERED
+
+function isDelivered(status: OrderStatus){
+    return status === OrderStatus.DELIVERED
+}
+
+isDelivered(OrderStatus.)
