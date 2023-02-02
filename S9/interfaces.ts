92b90eaa2a7ebb95d9d073cbd242e8bc4b2b5c75
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-02 23:00:54 +0900

    sec9 : readonly

--- S9/interfaces.ts
+++ S9/interfaces.ts
@@ -1,6 +1,13 @@
 interface Person {
-  name: string;
-  age: number;
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
 }
 
-const Aperson: Person = { name: "Son", age: 5 };
+const thomas: Person = {
+  id: 235235,
+  first: "Thomas",
+  last: "Hardy",
+  nickname: "gg",
+};
