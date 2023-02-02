963fba1dd15bd4acbe3e5195713d689e3c3ddf25
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-03 00:26:11 +0900

    sec10 : s

---
+++ S10/src/index.ts
@@ -0,0 +1,13 @@
+interface Chiken {
+  breed: string;
+  eggsPerWeek: number;
+  name: string;
+  age: number;
+}
+
+const norma: Chiken = {
+  breed: "Silkie",
+  eggsPerWeek: 4,
+  name: "Norma",
+  age: 20
+};
