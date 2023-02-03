5a283e280d63304cc771171aef4dd64c5624f360
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-03 11:16:30 +0900

    S11: mini project end

--- S11/dist/index.js
+++ S11/dist/index.js
@@ -1,6 +1,49 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+const todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
 }
-printDouble("HELLO ! ");
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    localStorage.setItem("todos", JSON.stringify(todos));
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLI = document.createElement("lI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", function () {
+//     alert(input.value)
+//     input.value = ""
+// })
+// 타입 단언
+// let mystery : unknown = "Helloe World !"
+// const numChars = (mystery as string).length
--- S11/src/index.ts
+++ S11/src/index.ts
@@ -1,6 +1,63 @@
-function printDouble(msg: string){
-    console.log(msg)
-    console.log(msg)
+interface Todo {
+  text: string;
+  completed: boolean;
 }
 
-printDouble("HELLO ! ")
+const btn = document.getElementById("btn")!;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist");
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  saveTodos();
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement("lI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+
+  newLI.append(todo.text);
+  newLI.append(checkbox);
+  list?.append(newLI);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// btn.addEventListener("click", function () {
+//     alert(input.value)
+//     input.value = ""
+// })
+
+// 타입 단언
+// let mystery : unknown = "Helloe World !"
+// const numChars = (mystery as string).length
