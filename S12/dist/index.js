b422643a4aff5ce0c386f46872c766f9bc498ab8
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-06 10:02:26 +0900

    S12: class field

--- S12/dist/index.js
+++ S12/dist/index.js
@@ -1,4 +1,8 @@
 class Player {
+    // 클래스 필드
+    score = 0;
+    numLives = 10;
+
     constructor(first, last){
         this.first = first
         this.last = last
@@ -9,10 +13,18 @@ class Player {
         console.log("WOOO!!")
     }
 
+    loseLife(){
+        this.numLives -= 1
+    }
+
 }
 
 const Player1 = new Player("SON", "HM")
 Player1.taunt()
+console.log(Player1.numLives)
+Player1.loseLife()
+console.log(Player1.numLives)
 
 const Player2 = new Player("Park", "JS")
 Player2.taunt()
+console.log(Player2)
