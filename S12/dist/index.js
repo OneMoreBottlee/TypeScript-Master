aa896c34091596a64bf7a829cd31c31ec8c63974
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-06 10:44:28 +0900

    S12: getter setter

--- S12/dist/index.js
+++ S12/dist/index.js
@@ -10,6 +10,27 @@ class Player {
         console.log(`I'm ${this.first} ${this.last}`)
     }
 
+    get fullName(){
+        return `${this.first} ${this.last}`
+    }
+
+    set fullName(newName){
+        const [first, last] = newName.split(" ")
+        this.first = first
+        this.last = last
+    }
+
+    get score(){
+        return this.#score
+    }
+
+    set score(newScore){
+        if(newScore < 0){
+            throw new Error("Score must be greater than ZERO !!")
+        }
+        this.#score = newScore
+    }
+
     getScore(){
         return this.#score
     }
@@ -34,11 +55,15 @@ class Player {
 
 const Player1 = new Player("SON", "HM")
 Player1.taunt()
-console.log(Player1.getScore())
-console.log(Player1.setScore(55))
-console.log(Player1.getScore())
+console.log(Player1.fullName)
+Player1.fullName = "Kim MJ"
 console.log(Player1)
+// console.log(Player1.score)
+// Player1.score = 353
+// console.log(Player1.score)
+// Player1.score = -353
+// console.log(Player1.score)
 
-const Player2 = new Player("Park", "JS")
-Player2.taunt()
-console.log(Player2)
+// const Player2 = new Player("Park", "JS")
+// Player2.taunt()
+// console.log(Player2)
