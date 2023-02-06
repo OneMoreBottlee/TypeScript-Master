22cd2182745e8b6446f017d80139329640d0976f
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-06 10:22:26 +0900

    S12: private

--- S12/dist/index.js
+++ S12/dist/index.js
@@ -1,12 +1,22 @@
 class Player {
     // 클래스 필드
-    score = 0;
+    #score = 0;
     numLives = 10;
 
     constructor(first, last){
         this.first = first
         this.last = last
+        this.#secret
         console.log(`I'm ${this.first} ${this.last}`)
+    }
+
+    getScore(){
+        return this.#score
+    }
+
+    setScore(newScore){
+        this.#score = newScore
+        console.log("score update!")
     }
 
     taunt(){
@@ -17,13 +27,17 @@ class Player {
         this.numLives -= 1
     }
 
+    #secret(){
+        console.log("Secret !!!")
+    }
 }
 
 const Player1 = new Player("SON", "HM")
 Player1.taunt()
-console.log(Player1.numLives)
-Player1.loseLife()
-console.log(Player1.numLives)
+console.log(Player1.getScore())
+console.log(Player1.setScore(55))
+console.log(Player1.getScore())
+console.log(Player1)
 
 const Player2 = new Player("Park", "JS")
 Player2.taunt()
