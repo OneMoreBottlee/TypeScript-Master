5bcfeac4d4d3c0d8063cb592112bc5736a7c7b1b
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-07 10:24:09 +0900

    sec13 : readonly

--- S13/dist/index.js
+++ S13/dist/index.js
@@ -1,8 +1,11 @@
 "use strict";
 class Player {
     constructor(first, last) {
+        this.score = 0;
         this.first = first;
         this.last = last;
     }
 }
 const koNo7 = new Player("HM", "SON");
+koNo7.score = 55;
+koNo7.first = "KIM";
--- S13/src/index.ts
+++ S13/src/index.ts
@@ -1,6 +1,7 @@
 class Player {
-  first: string;
-  last: string;
+  readonly first: string;
+  readonly last: string;
+  score:number = 0;
 
   constructor(first: string, last: string) {
     this.first = first;
@@ -11,3 +12,6 @@ class Player {
 }
 
 const koNo7 = new Player("HM", "SON")
+koNo7.score = 55
+koNo7.first = "KIM"
+
