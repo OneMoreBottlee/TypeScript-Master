b1987610039b19f802bce9ce36b406eb2a4fd5b9
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-07 10:41:14 +0900

    sec13 : 파라미터 프로퍼티 단축 구문

--- S13/dist/index.js
+++ S13/dist/index.js
@@ -1,19 +1,25 @@
 "use strict";
-var _Player_score;
+// class Player {
+//   public readonly first: string;
+//   public readonly last: string;
+//   private score:number = 0;
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//     this.secretMethod()
+//   }
+//   private secretMethod() :void{
+//     console.log("SECRET METHOD !")
+//   }
+// }
 class Player {
-    constructor(first, last) {
-        this.score = 0;
-        _Player_score.set(this, 0);
+    constructor(first, last, score) {
         this.first = first;
         this.last = last;
-        this.secretMethod();
+        this.score = score;
     }
     secretMethod() {
         console.log("SECRET METHOD !");
     }
 }
-_Player_score = new WeakMap();
-const koNo7 = new Player("HM", "SON");
-koNo7.score = 55;
-koNo7. = 55;
-koNo7.secretMethod();
+const koNo7 = new Player("HM", "SON", 100);
--- S13/src/index.ts
+++ S13/src/index.ts
@@ -1,21 +1,25 @@
+// class Player {
+//   public readonly first: string;
+//   public readonly last: string;
+//   private score:number = 0;
+
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//     this.secretMethod()
+//   }
+
+//   private secretMethod() :void{
+//     console.log("SECRET METHOD !")
+//   }
+// }
+
 class Player {
-  public readonly first: string;
-  public readonly last: string;
-  private score:number = 0;
-  #score: number = 0
-
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
-    this.secretMethod()
+    constructor(public first: string, public last: string, public score: number) {}
+  
+    private secretMethod() :void{
+      console.log("SECRET METHOD !")
+    }
   }
 
-  private secretMethod() :void{
-    console.log("SECRET METHOD !")
-  }
-}
-
-const koNo7 = new Player("HM", "SON")
-koNo7.score = 55
-koNo7.#score = 55
-koNo7.secretMethod()
+const koNo7 = new Player("HM", "SON", 100)
