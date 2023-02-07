3f910f5885623d31327f08123e8de447892ed8e9
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-07 10:36:57 +0900

    sec13 : public & private

--- S13/dist/index.js
+++ S13/dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
+var _Player_score;
 class Player {
     constructor(first, last) {
         this.score = 0;
+        _Player_score.set(this, 0);
         this.first = first;
         this.last = last;
+        this.secretMethod();
+    }
+    secretMethod() {
+        console.log("SECRET METHOD !");
     }
 }
+_Player_score = new WeakMap();
 const koNo7 = new Player("HM", "SON");
 koNo7.score = 55;
-koNo7.first = "KIM";
+koNo7. = 55;
+koNo7.secretMethod();
--- S13/src/index.ts
+++ S13/src/index.ts
@@ -1,17 +1,21 @@
 class Player {
-  readonly first: string;
-  readonly last: string;
-  score:number = 0;
+  public readonly first: string;
+  public readonly last: string;
+  private score:number = 0;
+  #score: number = 0
 
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
+    this.secretMethod()
   }
 
-
+  private secretMethod() :void{
+    console.log("SECRET METHOD !")
+  }
 }
 
 const koNo7 = new Player("HM", "SON")
 koNo7.score = 55
-koNo7.first = "KIM"
-
+koNo7.#score = 55
+koNo7.secretMethod()
