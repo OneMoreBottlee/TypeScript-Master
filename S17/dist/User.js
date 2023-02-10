3045888e17dcdb50e820740a4163b38670f6fc9b
OneMoreBottlee <hanbj0301@gmail.com>
2023-02-10 14:24:27 +0900

    sec17 : Module

---
+++ S17/dist/User.js
@@ -0,0 +1,13 @@
+export default class User {
+    constructor(username, email) {
+        this.username = username;
+        this.email = email;
+    }
+    ;
+    logout() {
+        console.log(`${this.username} logs out !!`);
+    }
+}
+export function userHelper() {
+    console.log("user");
+}
---
+++ S17/dist/index.js
@@ -0,0 +1,6 @@
+import { sample, add } from './utils.js';
+import User, { userHelper } from './User.js';
+add(1, 2);
+sample([15, 3, 3]);
+console.log(User);
+userHelper;
